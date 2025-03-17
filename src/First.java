822c04bbfd25643ab80f4f8b31a02c4c65a06b1c
eugene <evsurmyak@gmail.com>
2025-03-17 20:20:13 +0300

    fixed int height

--- src/First.java
+++ src/First.java
@@ -3,6 +3,7 @@ public class First {
     int age = 20;
     int height = 100;
     int weight = 200;
+    String color = "red";
 
     public static void main(String[] args) {
         System.out.println("Hello");
